7ce39f2a9034bb3f4fba95d4eccd4f6fcd0ea387
Yutaka Nakano <nknytk.dev@gmail.com>
2017-11-04 07:58:10 +0900

    画像サイズを揃えるスクリプト

---
+++ cnn/resize_crop.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+
+import os
+from PIL import Image
+
+#OUT_SIZE = 299
+OUT_SIZE = 224
+#OUT_SIZE = 100
+
+
+def main():
+    for f in os.listdir('jpg'):
+        img = Image.open('jpg/' + f)
+        w, h = img.size
+        # 縦横の長さが違う場合、短辺の長さまで長辺の両端を切り落とす
+        if w > h:
+            padding = int((w - h) / 2)
+            img = img.crop((padding, 0, h + padding, h))
+        elif w < h:
+            padding = int((h - w) / 2)
+            img = img.crop((0, padding, w, w + padding))
+        # リサイズして保存
+        img.thumbnail((OUT_SIZE, OUT_SIZE))
+        img.save('dataset{}/'.format(OUT_SIZE) + f)
+
+
+if __name__ == '__main__':
+    main()
